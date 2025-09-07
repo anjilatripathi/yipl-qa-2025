2f46f5aba08209aaff09d3f27cb800a381acd424
Anjila Tripathi <anjila.tripathi@gmail.com>
2025-09-07 22:08:44 +0545

    Setup

--- Automation/NoteAppAutomation/src/main/java/NoteAppAutomation.java
+++ Automation/NoteAppAutomation/src/main/java/NoteAppAutomation.java
@@ -1,7 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class NoteAppAutomation
 {
     public static void main(String[] args)
     {
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://practice.expandtesting.com/notes/app");
+        driver.quit();
+
 
     }
 
