39dffbd72d00131f7fbcf15e97a3992548f95cf1
Anjila Tripathi <anjila.tripathi@gmail.com>
2025-09-08 21:12:25 +0545

    Javascript Executor for scrolling the page

--- Automation/NoteAppAutomation/src/main/java/NoteAppAutomation.java
+++ Automation/NoteAppAutomation/src/main/java/NoteAppAutomation.java
@@ -1,5 +1,9 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -12,6 +16,15 @@ public class NoteAppAutomation
         WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.get("https://practice.expandtesting.com/notes/app");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        WebElement register = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Create an account")));
+        register.click();
         driver.quit();
 
 
